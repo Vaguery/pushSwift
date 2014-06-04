b7e55470a6edba3bad7ba32a484a312aefffdc3a
Bill Tozier <bill@vagueinnovation.com>
2014-06-04 14:37:32 -0400

    Initial Commit

---
+++ PushInterpreter/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  PushInterpreter
+//
+//  Created by Bill Tozier on 6/4/14.
+//  Copyright (c) 2014 Bill Tozier. All rights reserved.
+//
+
+import Cocoa
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+                            
+    @IBOutlet var window: NSWindow
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification?) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification?) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ PushInterpreter/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  PushInterpreter
+//
+//  Created by Bill Tozier on 6/4/14.
+//  Copyright (c) 2014 Bill Tozier. All rights reserved.
+//
+
+import Cocoa
+
+NSApplicationMain(C_ARGC, C_ARGV)
