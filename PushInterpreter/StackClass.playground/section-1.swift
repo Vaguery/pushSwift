0e2086fc2127b3e0e4d6f99d9f259cfa2f338a3c
Bill Tozier <bill@vagueinnovation.com>
2014-06-06 21:19:07 -0400

    Playground work trying to design spike a program point solution. Subclasses or Protocols?

--- PushInterpreter/StackClass.playground/section-1.swift
+++ PushInterpreter/StackClass.playground/section-1.swift
@@ -10,7 +10,7 @@ import XCTest
 // DeepCopyable
 
 @objc protocol DeepCopyable {
-    class func deep_copy() -> Self
+    func deep_copy() -> Self
 }
 
 
@@ -19,21 +19,20 @@ class NonconformingClass {
 
 
 class ImmediatelyConformingClass: DeepCopyable {
-    class func deep_copy() -> ImmediatelyConformingClass { return ImmediatelyConformingClass() }
+    func deep_copy() -> ImmediatelyConformingClass { return ImmediatelyConformingClass() }
 }
 
 
 class EventuallyConformingClass {
 }
 
-
 //struct ConformingStruct:DeepCopyable {
-//    mutating func deep_copy() -> ConformingStruct { return ConformingStruct() }
+//    func deep_copy() -> ConformingStruct { return ConformingStruct() }
 //}
 
 
 extension EventuallyConformingClass: DeepCopyable {
-    class func deep_copy() -> EventuallyConformingClass { return EventuallyConformingClass() }
+    func deep_copy() -> EventuallyConformingClass { return EventuallyConformingClass() }
 }
 
 
@@ -57,3 +56,31 @@ var myParser = PushParser()
 myParser.parseScript("foo bar")
 myParser.tokens
 
+// Tring to understand subclassed generics
+
+class GenericThing<T> {
+    var myValue = 88
+}
+
+class SomewhatLessGenericThing<T>:GenericThing<T> {
+    var myOtherValue = 999
+}
+
+let myThing = SomewhatLessGenericThing<Int>()
+myThing.myOtherValue = 2
+
+// Versus standard subclasses
+
+class CoreClassThing {
+    var myValue = 11
+}
+
+class SubclassedThing:CoreClassThing,DeepCopyable {
+    var myOtherValue = 22
+    func deep_copy() -> SubclassedThing {
+        return SubclassedThing()
+    }
+}
+
+let x = SubclassedThing()
+let y = SubclassedThing().deep_copy()
